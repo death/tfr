edff8caec3fe0d19142b8f2432bb870329f234ac
death <death@adeht.org>
2017-06-14 17:43:45 +0300

    read base directory from config file

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/jessevdk/go-flags"
+	"github.com/shibukawa/configdir"
 )
 
 type Options struct {
-	BaseDir string `long:"base-dir" description:"Base directory" default:"/media/1984/Documents/textfiles/"`
-	DBFile  string `long:"db-file" description:"Database file name" default:"/media/1984/Documents/textfiles/tfr.db"`
+	BaseDir string `long:"base-dir" description:"Base directory"`
+	DBFile  string `long:"db-file" description:"Database file name"`
 }
 
 var (
@@ -17,6 +20,20 @@ var (
 )
 
 func main() {
+	configDirs := configdir.New("adeht", "tfr")
+	configDir := configDirs.QueryFolderContainsFile("config")
+	if configDir != nil {
+		ini := flags.NewIniParser(parser)
+		if err := ini.ParseFile(filepath.Join(configDir.Path, "config")); err != nil {
+			log.Printf("config file error: %v\n", err)
+		}
+	}
+	if options.BaseDir == "" {
+		log.Fatal("base directory must be supplied via config file")
+	}
+	if options.DBFile == "" {
+		options.DBFile = filepath.Join(options.BaseDir, "tfr.db")
+	}
 	if _, err := parser.Parse(); err != nil {
 		os.Exit(1)
 	}
